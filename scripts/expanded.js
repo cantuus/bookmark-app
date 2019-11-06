69139330341e7eb7520a3afb4f11fcf126f05b63
Sophia Koeut <koeutsophia@gmail.com>
2019-11-06 14:33:43 -0800

    modifying delete button

--- scripts/expanded.js
+++ scripts/expanded.js
@@ -1,16 +1,36 @@
+import listeners from './eventlistener.js'
+
 const displayExpandedHtml = function (arrayItem, pageHtml) {
     let expandedHtml =
         `<div class="item-container" id="expanded">
-                    <div id="title-delete-button"><li class='bookmark-list-item' data-item-id='${arrayItem.id}'>${arrayItem.title}</li>
+                    <div id="title-delete-button" data-item-id='${arrayItem.id}><li class='bookmark-list-item'>${arrayItem.title}</li>
                     <button class="delete-bookmark"><i class="far fa-trash-alt"></i></button></div>
                     <div><a href='${arrayItem.url}'><button class="visit-site-bookmark">Visit Site</button></a></div>
                     <p>${arrayItem.description}
                     </p>
                 </div>`;
+
+    attachDeleteButton();
+        
     pageHtml = pageHtml + expandedHtml;
     return pageHtml;
 }
 
+const attachDeleteButton = function () {
+    $(document).ready(function () {
+        $('.item-container').on('click', '#title-delete-button', function (event) {
+            const id = listeners.getItemIdFromElement(event.currentTarget);
+            let foundItem = listeners.getItem(id);
+            console.log(foundItem);
+            $(this).parent().remove();
+            console.log('delete-button is clicked');
+        });
+    });
+}
+
+
+
 export default {
-    displayExpandedHtml
+    displayExpandedHtml,
+    attachDeleteButton
 }
